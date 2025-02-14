f917b1777815a523490924120069d9b43bfd453d
LEORIC\leoric <leoriclin@hotmail.com>
2025-02-15 00:05:29 +0800

    fix: update images

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,25 +1,27 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'MTG Card Finder',
-    short_name: 'MTG Finder',
-    description: 'Made this because the official app sucks',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
+    name: "MTG Card Finder",
+    short_name: "MTG Finder",
+    description: "Made this because the official app sucks",
+    start_url: "/",
+    display: "standalone",
     icons: [
       {
-        src: '/public/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
       },
       {
-        src: '/public/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
-  }
+    theme_color: "#000000",
+    background_color: "#000000",
+  };
 }
