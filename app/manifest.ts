74a8a08e4211bdbef1cf372840b0942b56d8b240
LEORIC\leoric <leoriclin@hotmail.com>
2025-02-15 02:25:28 +0800

    test: remove type

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,6 +1,4 @@
-import type { MetadataRoute } from "next";
-
-export default function manifest(): MetadataRoute.Manifest {
+export default function manifest() {
   return {
     name: "MTG Card Finder",
     short_name: "MTG Finder",
