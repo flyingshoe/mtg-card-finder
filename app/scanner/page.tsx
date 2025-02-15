2c44832baba88cb90308b9ea346951e4b52ef34c
LEORIC\leoric <leoriclin@hotmail.com>
2025-02-15 15:51:30 +0800

    test: fix errors

--- app/scanner/page.tsx
+++ app/scanner/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Tesseract from "tesseract.js";
 
 const CameraOCR = () => {
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [text, setText] = useState("");
   const [loading, setLoading] = useState(false);
-
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(typeof window !== "undefined" && !!navigator.mediaDevices);
+  }, []);
   // Start Camera
   const startCamera = async () => {
     try {
@@ -21,12 +24,24 @@ const CameraOCR = () => {
     }
   };
 
+  // Stop Camera
+  const stopCamera = () => {
+    if (videoRef.current && videoRef.current.srcObject) {
+      const stream = videoRef.current.srcObject as MediaStream;
+      const tracks = stream.getTracks();
+      tracks.forEach((track) => track.stop()); // Stop each track
+      videoRef.current.srcObject = null;
+    }
+  };
+
   // Capture Frame and Process OCR
   const captureImage = () => {
     if (videoRef.current && canvasRef.current) {
       const canvas = canvasRef.current;
       const context = canvas.getContext("2d");
-      context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+      if (context) {
+        context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+      }
 
       // Convert canvas to image data
       canvas.toBlob((blob) => {
@@ -36,7 +51,7 @@ const CameraOCR = () => {
   };
 
   // Process Image with Tesseract.js
-  const processImage = (image) => {
+  const processImage = (image: Tesseract.ImageLike) => {
     setLoading(true);
     Tesseract.recognize(image, "eng", {
       logger: (m) => console.log(m), // Logs progress
@@ -53,12 +68,21 @@ const CameraOCR = () => {
 
   return (
     <div className="p-4">
-      <button
-        onClick={startCamera}
-        className="px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Start Camera
-      </button>
+      {isClient ? "ISCLIENT" : "NOTCLIENT"}
+      <div className="flex gap-2">
+        <button
+          onClick={startCamera}
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          Start Camera
+        </button>
+        <button
+          onClick={stopCamera}
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          Stop Camera
+        </button>
+      </div>
 
       <div className="mt-4">
         <video
