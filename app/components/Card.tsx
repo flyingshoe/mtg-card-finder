bbaa4338834ec1669ba458ce52efbe12bddd2df6
flyingshoe <70445663+flyingshoe@users.noreply.github.com>
2025-05-09 17:20:51 +0800

    Fix: hide wrong cards

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -72,7 +72,7 @@ export default function MtgCard({ imageSrc, cardName }: CardProps) {
     setLoading(false);
     setShopList(
       res.data.data
-        .filter((card: ShopItemProps) => /* card.name.includes(cardName) && */ card.inStock)
+        .filter((card: ShopItemProps) => card.name.includes(cardName) && card.inStock)
         .sort((a: ShopItemProps, b: ShopItemProps) => a.price - b.price) // Sort by price in ascending order
         .slice(0, maxCards)
     );
